67dd9cf25b7bfcf4207dbae595542b5a3de6a3df
anjali-tripathi1 <anjalitripathi.kit@gmail.com>
2025-08-28 23:41:34 +0530

    upload

--- src/app/coustomDirectives/setBackground.directive.ts
+++ src/app/coustomDirectives/setBackground.directive.ts
@@ -1,17 +1,22 @@
 import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Renderer2 } from "@angular/core";
 
 @Directive({
    selector: '[setBackground]'
 })
 
 export class SetBackground implements OnInit{
-     constructor(private element:ElementRef){
+
+     constructor(private element:ElementRef, private randerer:Renderer2){
 
      }
 
      ngOnInit(){
-         this.element.nativeElement.style.backgroundColor = '#36454f'
-         this.element.nativeElement.style.color = 'white'
+        //  this.element.nativeElement.style.backgroundColor = '#36454f'
+        //  this.element.nativeElement.style.color = 'white'
+        this.randerer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454f')
+        this.randerer.setStyle(this.element.nativeElement, 'color', 'white')
+        this.randerer.setAttribute(this.element.nativeElement, 'title', 'Best Product')
      }
 }
 
